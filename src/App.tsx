e2ffa950b1fb566893f76d0e28923b0bbf4270bf
Zabo <zabo@Mac.fritz.box>
2025-08-18 20:34:14 +0200

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function ProductPage() {
 function App() {
     return (
         <PageLayout
-            title="ELYSION v1"
+            title="ELYSION v2"
             logo={<Leaf className="w-8 h-8 text-green-600" />}  // Das Logo als React-Komponente
         >
             <Routes>
